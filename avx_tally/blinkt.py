3712ea402d865902226798fbd36922c7b14f7290
James Muscat <jamesremuscat@gmail.com>
2020-10-26 15:03:18 +0000

    Improve client.

--- avx_tally/blinkt.py
+++ avx_tally/blinkt.py
@@ -1,12 +1,14 @@
 from __future__ import absolute_import
 from argparse import ArgumentParser
+from avx import PyroUtils
 from avx.Client import Client
 from avx.controller.Controller import Controller
 from avx.devices import Device
 from avx_tally import CONTROLLED_TALLY_MESSAGE, DEVICE_DEFAULT_METHODS
-from blinkt import clear, set_all, show
+from blinkt import clear, set_pixel, set_all, show, NUM_PIXELS
 
 import atexit
+import time
 
 
 class BlinktTally(Device):
@@ -25,14 +27,15 @@ class BlinktTally(Device):
 
     def tallyOff(self):
         clear()
+        show()
 
 
 class BlinktTallyClient(Client):
 
-    def __init__(self, tally_input):
+    def __init__(self, tally_input, brightness=0.1):
         super(BlinktTallyClient, self).__init__()
         self._tally_input = tally_input
-        self._device = BlinktTally('tally')
+        self._device = BlinktTally('tally', brightness=brightness)
 
     def handleMessage(self, msgType, source, payload):
         if msgType == CONTROLLED_TALLY_MESSAGE:
@@ -55,12 +58,28 @@ if __name__ == '__main__':
     parser.add_argument(
         '-t', '--tally', help='Tally input number to monitor', type=int, required=True
     )
+    parser.add_argument(
+        '-b', '--brightness', help='Brightness of LEDs, between 0 and 1',
+        type=float,
+        default=0.1
+    )
     args = parser.parse_args()
 
+    clear()
+    set_pixel(NUM_PIXELS - 1, 0, 0, 255, 0.05)
+    show()
+
     controller = Controller.fromPyro(args.controller)
-    client = BlinktTallyClient(args.tally)
+    client = BlinktTallyClient(args.tally, args.brightness)
+    client.setDaemon(True)
     client.start()
     client.started.wait()
     atexit.register(lambda: controller.unregisterClient(client.uri))
 
     controller.registerClient(client.uri)
+
+    try:
+        while True:
+            time.sleep(0.5)
+    except KeyboardInterrupt:
+        pass
